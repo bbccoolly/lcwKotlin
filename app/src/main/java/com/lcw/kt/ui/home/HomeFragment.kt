4c9090a7528e15da1aaee84f69a2228dfd28a3b7
Arrow <bbccoolly@gmail.com>
2021-09-24 14:47:33 +0800

    网络请求 v2

--- app/src/main/java/com/lcw/kt/ui/home/HomeFragment.kt
+++ app/src/main/java/com/lcw/kt/ui/home/HomeFragment.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
-import com.lcw.kt.R
 import com.lcw.kt.adapters.GalleryAdapter
 import com.lcw.kt.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
